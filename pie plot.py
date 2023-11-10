5c7093b8807c3dff12836a70ea4a4ab001c7ff67
usman21aex <149913288+usman21aex@users.noreply.github.com>
2023-11-10 10:08:23 -0800

    Add files via upload

---
+++ pie plot.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 10 07:19:27 2023
+
+@author: usman
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_pie_chart_covid_cases(dataframe, time_period):
+    """
+    Function to create a pie chart for the distribution of COVID cases among countries.
+
+    Arguments:
+    dataframe -- A pandas DataFrame with countries as rows and time periods as columns.
+    time_period -- A specific time period for which the pie chart is to be created.
+    """
+    plt.figure(figsize=(8, 8))
+
+    # Extract data for the selected time period
+    data_for_time_period = dataframe[time_period]
+
+    # Plotting the pie chart
+    plt.pie(data_for_time_period, labels=data_for_time_period.index, autopct='%1.1f%%', startangle=140, colors=['blue', 'red', 'orange'])
+
+    plt.title(f'Distribution of COVID Cases among Countries')
+
+    # Save the figure as an image file (e.g., PNG)
+    plt.savefig(f'Covid_Cases_Pie_Chart.png')
+
+    # Show the plot
+    plt.show()
+
+    return
+
+# Read data from Excel file
+file_path = r'C:/Users/Administrator/downloads/CovidComparison.xlsx'  # Using a raw string
+covid_df = pd.read_excel(file_path, index_col=0)
+
+# Choose a specific time period (e.g., the last one in the DataFrame)
+selected_time_period = covid_df.columns[0]
+
+# Call the pie chart function
+plot_pie_chart_covid_cases(covid_df, selected_time_period)
